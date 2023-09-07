ce806ca9c5c7608834338d92e2a227616a67f512
Justin Chu <justinchu24.com>
2023-09-07 18:21:38 -0400

    styled oh yeah

--- client/App.jsx
+++ client/App.jsx
@@ -6,11 +6,6 @@ function App() {
   return (
     <div id="app">
       <MainContainer />
-      <div>
-        Image by
-        {' '}
-        <a href="https://www.freepik.com/free-photo/side-view-fit-man-training-gym_40133040.htm#&position=0&from_view=collections">Freepik</a>
-      </div>
     </div>
   );
 }
